421d5e0d7d0fd55c9de8cb0f9f14d11d9bec02bf
mustafa-ozkan <mustafa.ozkan1241@gmail.com>
2020-10-17 11:19:53 -0400

    create mentor classes

---
+++ src/CybertekApp.java
@@ -0,0 +1,12 @@
+public class CybertekApp {
+
+    public static void main(String[] args) {
+        FullTimeMentor fullTimeMentor = new FullTimeMentor();
+        PartTimeMentor partTimeMentor = new PartTimeMentor();
+
+        Mentor mentor = new Mentor(fullTimeMentor,partTimeMentor);
+        mentor.manageAcount();
+
+    }
+
+}
