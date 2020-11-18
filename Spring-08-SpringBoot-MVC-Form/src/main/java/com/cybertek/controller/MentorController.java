c3f3cea23979f5554abaf38f5c9ac41c29241565
mustafa-ozkan <mustafa.ozkan1241@gmail.com>
2020-11-17 20:52:59 -0500

    add to mvc form with thymeleaf confirmation

--- Spring-08-SpringBoot-MVC-Form/src/main/java/com/cybertek/controller/MentorController.java
+++ Spring-08-SpringBoot-MVC-Form/src/main/java/com/cybertek/controller/MentorController.java
@@ -4,6 +4,8 @@ import com.cybertek.model.Mentor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Arrays;
@@ -20,6 +22,12 @@ public class MentorController {
         List<String> batchList = Arrays.asList("B7", "B8", "B9", "B10", "B11", "B12", "B13", "B14", "B15", "B16");
         model.addAttribute("batchList",batchList);
         return "mentor/mentor-register";
+    }
+
+    @PostMapping("/confirm")
+    public String submitForm(@ModelAttribute("mentor") Mentor mentor) {
+        System.out.println(mentor.toString());
+        return "mentor/mentor-confirmation";
     }
 
 }
