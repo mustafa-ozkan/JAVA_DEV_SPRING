6ecd43a25e0934a5460a4eb3c296527f8c6a26de
mustafa-ozkan <mustafa.ozkan1241@gmail.com>
2020-11-17 19:30:13 -0500

    add to mvc form with thymeleaf

---
+++ Spring-08-SpringBoot-MVC-Form/src/main/java/com/cybertek/controller/MentorController.java
@@ -0,0 +1,17 @@
+package com.cybertek.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/mentor")
+public class MentorController {
+
+//    @RequestMapping("/register")//accepting methods(get/post)which one should i use
+    @GetMapping("/mentor-register")
+    public String showForm(){
+        return "mentor/mentor-register";
+    }
+
+}
