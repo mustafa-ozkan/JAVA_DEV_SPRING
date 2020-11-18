12a1866e0cafb17d5b6c77d7d6a2aecc54eb9522
mustafa-ozkan <mustafa.ozkan1241@gmail.com>
2020-11-17 20:12:09 -0500

    add to mvc form with thymeleaf

--- Spring-08-SpringBoot-MVC-Form/src/main/java/com/cybertek/controller/MentorController.java
+++ Spring-08-SpringBoot-MVC-Form/src/main/java/com/cybertek/controller/MentorController.java
@@ -1,16 +1,24 @@
 package com.cybertek.controller;
 
+import com.cybertek.model.Mentor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping("/mentor")
 public class MentorController {
 
-//    @RequestMapping("/register")//accepting methods(get/post)which one should i use
-    @GetMapping("/mentor-register")
-    public String showForm(){
+    //    @RequestMapping("/register")//accepting methods(get/post)which one should i use
+    @GetMapping("/register")
+    public String showForm(Model model) {
+        model.addAttribute("mentor", new Mentor());
+        List<String> batchList = Arrays.asList("B7", "B8", "B9", "B10", "B11", "B12", "B13", "B14", "B15", "B16");
+        model.addAttribute("batchList",batchList);
         return "mentor/mentor-register";
     }
 
