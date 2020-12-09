c4e6304066273f06ac1fae416c21e449df2e6c54
mustafa-ozkan <mustafa.ozkan1241@gmail.com>
2020-12-08 21:02:17 -0500

    add bootstrap to mvc form

---
+++ Spring-09-SQL-Review/src/main/resources/functions.sql
@@ -0,0 +1,39 @@
+create or replace function get_department_count_by_name(dep_name varchar)
+    returns int
+    language plpgsql
+as
+$$
+declare
+    department_count integer;
+begin
+    select count(*)
+    into department_count
+    from employees
+    where department = dep_name;
+    return department_count;
+end;
+$$
+
+
+select get_department_count_by_name('Sports');
+
+create or replace function get_department(p_pattern varchar)
+    returns table
+            (
+                employee_name  varchar,
+                employee_email varchar
+            )
+    language plpgsql
+as
+$$
+begin
+    return query
+        select first_name, email
+        from employees
+        where department like p_pattern;
+end;
+$$
+
+select * from get_department('%oth%');
+
+drop function if exists ()
---
+++ Spring-09-SQL-Review/src/main/resources/procedures.sql
@@ -0,0 +1,48 @@
+create or replace procedure update_department(emp_id int)
+    language plpgsql
+as
+$$
+declare
+begin
+    update employees set department = 'Toys' where employee_id = emp_id;
+    commit;
+end
+$$;
+
+
+call update_department(1);
+
+select *
+from employees
+where employee_id = 1;
+
+drop function if exists get_department(p_pattern varchar);
+
+--write a query  procedure for changing salaries of two employees
+--
+
+
+create or replace procedure transfer(sender int, receiver int, amount dec)
+    language plpgsql
+as
+$$
+declare
+begin
+    update employees
+    set salary = salary - amount
+    where employee_id = sender;
+
+    update employees
+    set salary = salary + amount
+    where employee_id = receiver;
+    commit;
+end
+$$;
+
+select * from employees order by employee_id;
+
+call transfer(1,2,44000);
+
+select * from employees order by employee_id;
+
+drop procedure if exists update_salary_for_two_employee(sender integer, receiver integer, amount numeric);
