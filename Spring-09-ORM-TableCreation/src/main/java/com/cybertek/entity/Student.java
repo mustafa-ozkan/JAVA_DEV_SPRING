5d0eacdff2c7be4340bc94884e36ed5910715d11
mustafa-ozkan <mustafa.ozkan1241@gmail.com>
2020-12-12 14:05:38 -0500

    add ORM Module Table creation temporals

--- Spring-09-ORM-TableCreation/src/main/java/com/cybertek/entity/Student.java
+++ Spring-09-ORM-TableCreation/src/main/java/com/cybertek/entity/Student.java
@@ -1,6 +1,8 @@
 package com.cybertek.entity;
 
 import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.Date;
 
 @Entity
 @Table(name = "students")
@@ -9,8 +11,30 @@ public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long studentId;
+    @Column(name = "studentFirstName")
     private String firstName;//first_name in the database by hybernate
+    @Column(name = "studentLastName")
     private String lastName;
+    @Column(name = "studentEmailAddress")
     private String email;
+
+    @Transient
+    private String city;
+
+    @Temporal(TemporalType.DATE)
+    private Date birthDate;
+    @Temporal(TemporalType.TIME)
+    private Date birthTime;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date birthDateTime;
+
+    @Column(columnDefinition = "DATE")
+    private LocalDate localDate;
+    @Column(columnDefinition = "TIME")
+    private LocalDate localTime;
+    @Column(columnDefinition = "TIMESTAMP")
+    private LocalDate localDateTime;
+
+
 
 }
--- Spring-09-ORM-TableCreation/src/main/resources/data.sql
+++ Spring-09-ORM-TableCreation/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-insert into students(first_name,last_name,email) VALUES ('MIKE','SMITH','m@s')
-insert into students(first_name,last_name,email) VALUES ('Adam','Eve','a@e')
-insert into students(first_name,last_name,email) VALUES ('Kat','Dog','k@d')
-insert into students(first_name,last_name,email) VALUES ('Men','Women','m@W')
+insert into students(student_first_name,student_last_name,student_email_address) VALUES ('MIKE','SMITH','m@s')
+insert into students(student_first_name,student_last_name,student_email_address) VALUES ('Adam','Eve','a@e')
+insert into students(student_first_name,student_last_name,student_email_address) VALUES ('Kat','Dog','k@d')
+insert into students(student_first_name,student_last_name,student_email_address) VALUES ('Men','Women','m@W')
