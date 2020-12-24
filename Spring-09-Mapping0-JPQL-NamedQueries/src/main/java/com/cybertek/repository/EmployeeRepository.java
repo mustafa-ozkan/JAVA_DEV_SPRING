fe9dacac011155223520d6d4d3a306a5d8c0c8a1
mustafa-ozkan <mustafa.ozkan1241@gmail.com>
2020-12-23 19:51:10 -0500

    add ORM Lab Examples for Named Queries for Jpa

--- Spring-09-Mapping0-JPQL-NamedQueries/src/main/java/com/cybertek/repository/EmployeeRepository.java
+++ Spring-09-Mapping0-JPQL-NamedQueries/src/main/java/com/cybertek/repository/EmployeeRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -28,5 +30,44 @@ public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     //single named parameter
     @Query("select e from Employee e where e.salary=:salary")
     Employee getEmployeeBySalary(@Param("salary") int salary);
+
+    //multiple named parameters
+    @Query("select e from Employee e where e.firstName=:name or e.salary=:salary")
+    List<Employee> getEmployeeByFirstNameOrSalary(@Param("salary") int salary, @Param("name") String firstName);
+
+
+    //not equal
+    @Query("select e from Employee e where e.salary <> :salary")
+    List<Employee> getEmployeeBySalaryNotEqual(@Param("salary") int salary);
+
+    //Like/contains/startswith/ends with
+    @Query("select e from Employee e where e.firstName like ?1")
+    List<Employee> getEmployeeByFirstNameLike(String pattern);
+
+    //less than
+    @Query("select e from Employee e where e.salary, ?1")
+    List<Employee> getEmployeeBySalaryLessThan(int salary);
+
+    //greater than
+    @Query("select e from Employee e where e.salary>?1")
+    List<Employee> getEmployeeBySalaryGreaterThan(int salary);
+
+    //between
+    @Query("select e from Employee e where e.salary is between ?1 and ?2")
+    List<Employee> getEmployeeBySalaryBetween(int minSalary, int maxSalary);
+
+    //before
+    @Query("select e from Employee e where e.hireDate>?1")
+    List<Employee> getEmployeeByHireDateBefore(LocalDate date);
+
+    //null
+    @Query("select e from Employee e where e.email is null")
+    List<Employee> getEmployeeByEmailIsNull();
+
+    //notnull
+    @Query("select e from Employee e where e.email is not null")
+    List<Employee> getEmployeeByEmailIsNotNull();
+
+
 
 }
