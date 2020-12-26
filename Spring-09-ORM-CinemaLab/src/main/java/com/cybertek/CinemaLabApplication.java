8c55ae8afb15ba36a080666677c1bf6d43c64f5c
mustafa-ozkan <mustafa.ozkan1241@gmail.com>
2020-12-26 11:47:58 -0500

    practice queries CinemaLabApplication

--- Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/CinemaLabApplication.java
+++ Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/CinemaLabApplication.java
@@ -1,13 +1,37 @@
 package com.cybertek;
 
+import com.cybertek.repository.AccountDetailRepository;
+import com.cybertek.repository.CinemaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 public class CinemaLabApplication {
+    @Autowired
+    AccountDetailRepository accountDetailRepository;
+    @Autowired
+    CinemaRepository cinemaRepository;
 
-	public static void main(String[] args) {
-		SpringApplication.run(CinemaLabApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaLabApplication.class, args);
+    }
+
+    @PostConstruct
+    public void testAccountDetailsSql(){
+
+    }
+
+    @PostConstruct
+    public void testCinemaSql(){
+
+    }
+
+    @PostConstruct
+    public void testGenreSql(){
+
+    }
 
 }
