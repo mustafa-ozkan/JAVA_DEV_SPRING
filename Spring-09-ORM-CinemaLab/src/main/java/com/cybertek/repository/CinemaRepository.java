80eaccff26b3ed70fdbcca8bbad440b70b23ed7d
mustafa-ozkan <mustafa.ozkan1241@gmail.com>
2020-12-26 11:39:32 -0500

    add ORM Lab Examples for Named Queries for Jpa

--- Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/repository/CinemaRepository.java
+++ Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/repository/CinemaRepository.java
@@ -2,6 +2,7 @@ package com.cybertek.repository;
 
 import com.cybertek.entity.Cinema;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -29,10 +30,24 @@ List<Cinema> findAllByLocationCountry(String country);
 // ------------------- JPQL QUERIES ------------------- //
 
 //Write a JPQL query to read the cinema name with a specific id
+    @Query("select c from Cinema c where c.id=?1")
+    String fetchByIdJPQL(Integer id);
+
 // ------------------- Native QUERIES ------------------- //
 //Write a native query to read all cinemas by location country
+    @Query(value = "select * from cinema c join location l on l.id=c.location_id where l.country=?1", nativeQuery = true)
+    List<Cinema> retrieveAllByLocationCountry(String locationCountry);
 //Write a native query to read all cinemas by name or sponsored name contains a specific pattern
+    @Query(value = "select * from cinema where name ilike concat('%',?1,'%') or" +
+            " sponsored_name ilike concat('%',?1,'%') ", nativeQuery = true)
+    List<Cinema> retrievAllByNameOrSponsoredName(String pattern);
 //Write a native query to sort all cinemas by name
+
+    @Query(value = "select * from cinema order by name", nativeQuery = true)
+    List<Cinema> sortByName();
 //Write a native query to distinct all cinemas by sponsored name
+
+    @Query(value = "select distinct sponsored_name from cinema", nativeQuery = true)
+    List<String> distinctBySponsoredName();
 
 }
