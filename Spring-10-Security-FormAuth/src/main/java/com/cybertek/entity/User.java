a8b7d7657cf8f114baf398969d2ef35e3531233b
mustafa-ozkan <mustafa.ozkan1241@gmail.com>
2021-01-19 19:38:28 -0500

    create security form authorization

---
+++ Spring-10-Security-FormAuth/src/main/java/com/cybertek/entity/User.java
@@ -0,0 +1,47 @@
+package com.cybertek.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private int active;
+    private String roles;//"ADMIN,MANAGER,USER"
+    private String permissions;
+
+    //id will be created by postgres
+    public User(String username, String password, String roles, String permissions) {
+        this.username = username;
+        this.password = password;
+        this.roles = roles;
+        this.permissions = permissions;
+    }
+
+    public List<String> getRoleList(){
+        if(this.roles.length()>0){
+            return Arrays.asList(this.roles.split(","));
+        }
+        return new ArrayList<>();
+    }
+
+    public List<String> getPermissionList(){
+        if(this.permissions.length()>0){
+            return Arrays.asList(this.permissions.split(","));
+        }
+        return new ArrayList<>();
+    }
+}
---
+++ Spring-10-Security-HttpBasic-AutRoleBase/src/main/java/com/cybertek/entity/User.java
@@ -0,0 +1,5 @@
+package com.cybertek.entity;
+
+public class User {
+
+}
