41f484fb123f4939bf2d567e5e1227c4d9f04a5b
mustafa-ozkan <mustafa.ozkan1241@gmail.com>
2021-01-25 14:40:41 -0500

    create rest http legacy

---
+++ Spring-11-Rest-HttpVerbs-Legacy/src/main/java/com/cybertek/implementation/ProductServiceImpl.java
@@ -0,0 +1,37 @@
+package com.cybertek.implementation;
+
+import com.cybertek.entity.Product;
+import com.cybertek.service.ProductService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+
+    @Override
+    public List<Product> getProducts() {
+        return null;
+    }
+
+    @Override
+    public List<Product> delete(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Product> updateProduct(Long id, Product product) {
+        return null;
+    }
+
+    @Override
+    public List<Product> createProduct(Product product) {
+        return null;
+    }
+
+    @Override
+    public Product getProduct(Long id) {
+        return null;
+    }
+}
