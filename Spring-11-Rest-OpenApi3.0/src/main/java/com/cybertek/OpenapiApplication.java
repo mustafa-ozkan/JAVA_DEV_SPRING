26b6d0cee5d394e2a380da8d5aeb2401ce8b3253
mustafa-ozkan <mustafa.ozkan1241@gmail.com>
2021-02-06 11:00:17 -0500

    create rest security jwt

--- Spring-11-Rest-OpenApi3.0/src/main/java/com/cybertek/OpenapiApplication.java
+++ Spring-11-Rest-OpenApi3.0/src/main/java/com/cybertek/OpenapiApplication.java
@@ -1,7 +1,10 @@
 package com.cybertek;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class OpenapiApplication {
@@ -10,4 +13,12 @@ public class OpenapiApplication {
 		SpringApplication.run(OpenapiApplication.class, args);
 	}
 
+
+	@Bean
+	public OpenAPI customOpenApi(){
+		return new OpenAPI()
+				.info(new Info().title("Cinema Application")
+				.version("v1")
+				.description("Selling online cinema tickets"));
+	}
 }
