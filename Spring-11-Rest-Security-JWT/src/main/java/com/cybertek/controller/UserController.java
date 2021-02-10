2e7dff1e0e3351ab70cc1d514c5a4087bc91fd45
mustafa-ozkan <mustafa.ozkan1241@gmail.com>
2021-02-09 20:51:55 -0500

    create rest security jwt

--- Spring-11-Rest-Security-JWT/src/main/java/com/cybertek/controller/UserController.java
+++ Spring-11-Rest-Security-JWT/src/main/java/com/cybertek/controller/UserController.java
@@ -5,6 +5,8 @@ import com.cybertek.entity.User;
 import com.cybertek.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,12 +15,14 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/users")
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class UserController {
 
     @Autowired
     private UserService userService;
 
     @GetMapping("/read")
+    @PreAuthorize("hasAuthority('USER')")
     public ResponseEntity<ResponseWrapper> readAll(){
         List<User> users = userService.getAll();
 
