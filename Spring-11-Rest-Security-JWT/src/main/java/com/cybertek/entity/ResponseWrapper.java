f0f102ba43bc61eb74cc45e7c4a2d973e96257b9
mustafa-ozkan <mustafa.ozkan1241@gmail.com>
2021-02-10 19:31:00 -0500

    create rest security jwt

--- Spring-11-Rest-Security-JWT/src/main/java/com/cybertek/entity/ResponseWrapper.java
+++ Spring-11-Rest-Security-JWT/src/main/java/com/cybertek/entity/ResponseWrapper.java
@@ -8,14 +8,12 @@ Basically to show some message in api result
 if you delete something ....
  */
 import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.http.HttpStatus;
 
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
