c71d370e26b11b359159ffbd8f8a18bb452c6413
mustafa-ozkan <mustafa.ozkan1241@gmail.com>
2021-02-23 20:01:15 -0500

    create LoggingAspect and pointcuts

--- Spring-12-AOP/src/main/java/com/cybertek/aspects/LoggingAspect.java
+++ Spring-12-AOP/src/main/java/com/cybertek/aspects/LoggingAspect.java
@@ -3,7 +3,6 @@ package com.cybertek.aspects;
 
 import com.cybertek.controller.ProductController;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,12 +13,30 @@ import org.springframework.context.annotation.Configuration;
 public class LoggingAspect {
 
     Logger logger = LoggerFactory.getLogger(ProductController.class);
+//1st way
+//    @Pointcut("execution(* com.cybertek.controller.ProductController.*(..))")
+//    public void pointcut(){}
+//
+//    @Before("pointcut()")
+//    public void log(){
+//        logger.info("--------------");
+//    }
+//
+//2nd way
+//    @Before("execution(* com.cybertek.controller.ProductController.*(..))")
+//    public void beforeAdvice(){
+//        logger.info("--------------");
+//    }
+//    execution
 
-    @Pointcut("execution(* com.cybertek.controller.ProductController.*(..))")
-    public void pointcut(){}
+    @Pointcut("execution(* com.cybertek.controller.ProductController.up*(..))")
+    public void anyUpdateOperation(){}
 
-    @Before("pointcut()")
-    public void log(){
-        logger.info("--------------");
-    }
+    @Pointcut("execution(* com.cybertek.repository.ProductRepository.findById(Long))")
+    public void anyProductRepositoryFindById(){}
+
+
+
+
+
 }
