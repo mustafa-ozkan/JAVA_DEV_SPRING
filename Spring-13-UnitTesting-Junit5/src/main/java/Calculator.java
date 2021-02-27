f0e54e41e06eb3cd41a292e9b91397907ee4fdbf
mustafa-ozkan <mustafa.ozkan1241@gmail.com>
2021-02-27 11:17:52 -0500

    create unit testing junit5

--- Spring-13-UnitTesting-Junit5/src/main/java/Calculator.java
+++ Spring-13-UnitTesting-Junit5/src/main/java/Calculator.java
@@ -1,10 +1,12 @@
 public class Calculator {
 
 
-    public static int add(int num1, int num2){
+    public static String operator = "add";
 
-        return num1+num2;
+
+    public static int add(int num1, int num2) {
+
+        return num1 + num2;
     }
-
 
 }
--- Spring-13-UnitTesting-Junit5/src/test/java/com/testing/CybertekApplicationTests.java
+++ Spring-13-UnitTesting-Junit5/src/test/java/com/testing/CybertekApplicationTests.java
@@ -1,9 +1,6 @@
 package com.testing;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class CybertekApplicationTests {
 
 	@Test
