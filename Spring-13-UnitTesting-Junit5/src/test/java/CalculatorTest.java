32d0dfb27b4121b0378f66f53d229de849f662b0
mustafa-ozkan <mustafa.ozkan1241@gmail.com>
2021-02-27 11:24:19 -0500

    create unit testing junit5

--- Spring-13-UnitTesting-Junit5/src/test/java/CalculatorTest.java
+++ Spring-13-UnitTesting-Junit5/src/test/java/CalculatorTest.java
@@ -15,8 +15,24 @@ class CalculatorTest {
     }
 
     @Test
+    void testCase3() {
+        assertArrayEquals(new int[]{1,2,3}, new int[]{1,2,3});
+    }
+
+    @Test
+    void testCase4() {
+
+        String nullString =  null;
+        String notNullString = "Cybertek";
+//        assertNotNull(notNullString);
+//        assertNotNull(nullString);
+        assertNull(nullString);
+    }
+
+    @Test
     void add() {
         int actual= Calculator.add(2,3);
         assertEquals(5, actual);
     }
+
 }
